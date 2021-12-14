181d1d870d4c075921ac2cab966a1577aacc6740
spidy9869 <63071549+spidy9869@users.noreply.github.com>
2021-12-15 00:40:54 +0530

    Html done

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React , {useState , useEffect}from 'react';
+
+import  './style.css'
+
+
+
+function App() {
+return (
+ <div className="col">
+
+
+   <div className='ans'> 
+   
+   <div className="upper">upper</div>
+   <div className="lower">lower</div>
+   </div>
+   <div className="span2">AC</div>
+   <div>DEL</div>
+   <div>%</div>
+   <div >1</div>
+   <div> 2</div>
+   <div>3</div>
+   <div>*</div>
+   <div>4</div>
+   <div >5</div>
+   <div>6</div>
+   <div>+</div>
+   <div>7</div>
+   <div>8</div>
+   <div>9</div>
+   <div>-</div>
+   <div>.</div>
+   <div>0</div>
+   <div className="span2">=</div>
+
+ </div>
+);
+}
+
+export default App;

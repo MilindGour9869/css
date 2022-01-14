66ae2585d46effe3ba684d045d704ede3b3f051c
spidy9869 <63071549+spidy9869@users.noreply.github.com>
2022-01-14 14:42:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,38 +1,33 @@
-import React , {useState , useEffect}from 'react';
+import React , {useReducer}from 'react';
 
 import  './style.css'
 
-
+ 
 
 function App() {
+
+  function reducer(){
+    
+  }
+
+  const [ state , fun] = useReducer(reducer , { upr : "" , main :"" , op:""} )
+
+
 return (
- <div className="col">
-
-
-   <div className='ans'> 
-   
-   <div className="upper">upper</div>
-   <div className="lower">111111111111111111111111111111111</div>
+ <div className="div1">
+   <div className='img'>
+   <img src="https://forbitads.files.wordpress.com/2017/06/cropped-MDN-Creations-Group-Icon.png"></img>
    </div>
-   <div className="span2">AC</div>
-   <div>DEL</div>
-   <div>%</div>
-   <div >1</div>
-   <div> 2</div>
-   <div>3</div>
-   <div>*</div>
-   <div>4</div>
-   <div >5</div>
-   <div>6</div>
-   <div>+</div>
-   <div>7</div>
-   <div>8</div>
-   <div>9</div>
-   <div>-</div>
-   <div>.</div>
-   <div>0</div>
-   <div className="span2">=</div>
 
+
+   <div className='div2'>
+     
+     <p className='a'> qasasax acc
+     </p>
+
+
+     </div>
+   
  </div>
 );
 }
